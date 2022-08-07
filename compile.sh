f9c09c91ec33c75cba884c5b5ea4a89732f2b0f8
Stephan Göppentin <dedda102@gmail.com>
2022-08-07 17:25:51 +0200

    refurbished compile script

--- compile.sh
+++ compile.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-nasm -felf64 game.asm && \
-nasm -felf64 arrays.asm && \
-nasm -felf64 printing.asm && \
-ld -o game \
-  game.o \
-  arrays.o \
-  printing.o && \
+set -e
+
+source_files=( game arrays printing )
+linker_command="ld -o game"
+
+for source_file in "${source_files[@]}"
+do
+    echo "compiling ${source_file}.asm ..."
+    nasm -felf64 "${source_file}.asm"
+    linker_command="${linker_command} ${source_file}.o"    
+done
+
+echo "Linkig with command:"
+echo "  ${linker_command}"
+$($linker_command)
+
 rm *.o
