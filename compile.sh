ab14781fe6aff6fe1274b5627c1db03bdc9585df
Stephan Göppentin <dedda102@gmail.com>
2022-08-31 22:07:05 +0200

    not so elegant solution for trading and feeding bait

--- compile.sh
+++ compile.sh
@@ -4,6 +4,9 @@ set -e
 source_files=( main arrays game game_state input menu printing savegame room_docks room_harbor_district_plaza )
 linker_command="ld -o game"
 
+echo "Compiling source files:"
+echo ""
+
 for source_file in "${source_files[@]}"
 do
     echo "compiling ${source_file}.asm ..."
@@ -11,8 +14,10 @@ do
     linker_command="${linker_command} ${source_file}.o"    
 done
 
+echo ""
 echo "Linkig with command:"
 echo "  ${linker_command}"
+echo ""
 $($linker_command)
 
 rm *.o
--- room_docks.asm
+++ room_docks.asm
@@ -10,24 +10,32 @@ section .data
                           db 10, 0
 
     _msg_room_menu db "  [", green(look_around_txt), "] Look around", 10
+                   db "  [", green("give"), " (item) ", green("to"), " (recipient)] Give an item from your inventory", 10
                    db "  [", green(exit_txt), "] Leave the room (and this game)", 10, "> "
                    db 0
 
     _msg_look_around db "To your right, you see a fisherman. He looks desperate. Probably because he hasn't "
                      db "caught a single fish today.", 10
                      db "To your left is a gate that leeds out the docks. It doesn't appear to be locked but "
-                     db "but it is guarded by a very angry looking crow."
+                     db "but it is guarded by a very angry looking 🐦 Bird."
                      db 10, 0
 
     _give_fisherman_fish_for_bait_msg db "You trade 1 🐟 Fish for 1 🪱 Bait.", 10, 0
-    _give_fisherman_fish_for_bait_no_fish_msg db "You don't have any 🐟 Fish to trade.", 10, 0
+    _give_fisherman_fish_for_bait_no_fish_msg db "You don't have any 🐟 Fish to give.", 10, 0
+    _msg_give_bird_fish db "The 🐦 Bird does not want your 🐟 Fish.", 10, 0
 
     _feed_bird_msg db "You feed the 🐦 Bird a 🪱 Worm. It is happy and flies away.", 10, 0
     _feed_bird_no_bait_msg db "You don't have anything to feed the 🐦 Bird.", 10, 0    
 
     _menu_docks_look_around_text db "look", 10, 0
+    _menu_give_fisherman_fish_text db "give fish to fisherman", 10, 0
+    _menu_give_bird_fish_text db "give fish to bird", 10, 0
+    _menu_give_bird_bait_text db "give bait to bird", 10, 0
     _menu_docks_exit_text db "exit", 10, 0
     _menu_docks_data dq _menu_docks_look_around_text, _look_around
+                     dq _menu_give_fisherman_fish_text, _selected_give_fish_to_fisherman                     
+                     dq _menu_give_bird_fish_text, _selected_give_fish_to_bird
+                     dq _menu_give_bird_bait_text, _selected_give_bait_to_bird
                      dq _menu_docks_exit_text, _exit_docks
                      dq 0
 
@@ -49,7 +57,7 @@ extern print_newline
 extern read_line
 
 ; menu.asm
-extern run_basic_menu
+extern run_menu_with_meta_commands
 
 section .text
 
@@ -62,11 +70,22 @@ _room_docks:
     call read_line
     mov rsi, rax
     mov rdi, _menu_docks_data
-    call run_basic_menu
+    call run_menu_with_meta_commands
+    cmp rax, 0
+    jz _room_docks
     jmp rax
 _look_around:
     mov rdi, _msg_look_around
     call print_c_string
+    jmp _room_docks
+_selected_give_fish_to_fisherman:
+    call _give_fisherman_fish_for_bait
+    jmp _room_docks
+_selected_give_fish_to_bird:
+    call _give_bird_fish
+    jmp _room_docks
+_selected_give_bait_to_bird:
+    call _feed_bird
     jmp _room_docks
 _exit_docks:
     mov rax, 0xFF
@@ -83,11 +102,18 @@ _give_fisherman_fish_for_bait:
     jz _cannot_give_fish
     dec qword [fish_count]
     inc qword [bait_count]    
-    mov rsi, _give_fisherman_fish_for_bait_msg    
+    mov rdi, _give_fisherman_fish_for_bait_msg    
     call print_c_string
     ret
 _cannot_give_fish:
     mov rsi, _give_fisherman_fish_for_bait_no_fish_msg
+    call print_c_string
+    ret
+
+_give_bird_fish:
+    cmp qword [fish_count], 0
+    jz _cannot_give_fish
+    mov rdi, _msg_give_bird_fish
     call print_c_string
     ret
 
@@ -96,10 +122,10 @@ _feed_bird:
     jz _cannot_feed_bird
     dec qword [bait_count]
     mov byte [fed_bird], 1
-    mov rsi, _feed_bird_msg
+    mov rdi, _feed_bird_msg
     call print_c_string
     ret
 _cannot_feed_bird:
-    mov rsi, _feed_bird_no_bait_msg
+    mov rdi, _feed_bird_no_bait_msg
     call print_c_string
     ret
