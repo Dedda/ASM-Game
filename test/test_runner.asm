4470488b2c2f860dd92e101272ea4bc58093b672
Stephan Göppentin <dedda102@gmail.com>
2023-01-05 18:51:03 +0100

    simple test success and failure counter

--- test/test_runner.asm
+++ test/test_runner.asm
@@ -10,9 +10,12 @@ section .data
     _lbl_after_test_name db "...", 0
     _lbl_ok db "  [OK]", 10, 0
     _lbl_fail db "  [FAILURE]", 10, 0
+    _lbl_success_counter db "tests successful: ", 0
+    _lbl_failure_counter db "tests failed: ", 0
 
 extern print_c_string
 extern print_newline
+extern print_u64
 
 extern test_char_to_lowercase
 extern lbl_test_char_to_lowercase
@@ -28,12 +31,17 @@ extern lbl_test_c_strings_equal_ci
 
 global _start
 
+section .bss
+    _success_counter: resq 1
+    _failure_counter: resq 1
+
 section .text
 
 _start:
+    mov qword [_success_counter], 0
+    mov qword [_failure_counter], 0
     mov rdi, _label_start
     call print_c_string
-
     mov r8, _tests
 _next_test:
     cmp qword [r8], 0
@@ -46,6 +54,17 @@ _next_test:
     add r8, 16
     jmp _next_test
 _all_tests_run:
+    call print_newline
+    mov rdi, _lbl_success_counter
+    call print_c_string
+    mov rdi, [_success_counter]
+    call print_u64
+    call print_newline
+    mov rdi, _lbl_failure_counter
+    call print_c_string
+    mov rdi, [_failure_counter]
+    call print_u64
+    call print_newline
     call exit_group
 
 run_test:
@@ -61,10 +80,12 @@ run_test:
     call rdi
     cmp rax, 0
     jz _print_ok_lbl
+    inc qword [_failure_counter]
     mov rdi, _lbl_fail
     call print_c_string
     ret
 _print_ok_lbl:
+    inc qword [_success_counter]
     mov rdi, _lbl_ok
     call print_c_string
     ret
